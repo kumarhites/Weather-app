b32d5b707aaa90561b20c7bc94ee4ddefec33aa8
kumarhites <hkhiteshkumar66@gmail.com>
2022-01-09 16:59:41 +0530

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = "uiGfIvA1sXQTwRj8lgGMJPZx1vnBGZPG";
 
 // get weather info
 const getWeather = async(id) => {
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -15,7 +15,7 @@ const getWeather = async(id) => {
 
 // get city info
 const getCity = async (city) =>{
-    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
