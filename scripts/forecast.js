af0491d95282cd0aa16ea5047d27708b66f50c3e
Hitesh Kumar <hkhiteshkumar66@gmail.com>
2022-01-16 14:09:18 +0530

    committed

---
+++ scripts/forecast.js
@@ -0,0 +1,34 @@
+const key = "uiGfIvA1sXQTwRj8lgGMJPZx1vnBGZPG";
+
+// get weather info
+const getWeather = async(id) => {
+    const base = "https://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+
+};
+
+
+
+// get city info
+const getCity = async (city) =>{
+    const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// getCity('jamshedpur')
+//     .then(data => {
+//         return getWeather(data.Key);
+//     })
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
